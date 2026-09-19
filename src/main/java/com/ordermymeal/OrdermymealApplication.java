5c9da2ba3705b9e7e0035d496c46ee2daeca9705
vishnu <vishnuvardhanyendluri4@gmail.com>
2026-09-19 22:47:55 +0530

    Initialize OrderMyMeal repository structure

---
+++ src/main/java/com/ordermymeal/OrdermymealApplication.java
@@ -0,0 +1,13 @@
+package com.ordermymeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdermymealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdermymealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ordermymeal/OrdermymealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ordermymeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdermymealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
